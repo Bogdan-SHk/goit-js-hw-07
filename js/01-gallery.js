9431db5e8db8fda9c4933613a1e55087b7f0e83a
Bogdan-SHk <bogdan.shkolnik@gmail.com>
2023-02-06 22:09:55 +0200

    Виконав завдання 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+// Створення і рендер розмітки
+
+const createGalleryMarkup = galleryItems => {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+  }).join('');
+};
+
+const galleryContainer = document.querySelector(".gallery");
+
+const cardMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardMarkup);
+
+
+// Реалізація делегування на div.gallery, відкриття та закриття модалки
+
+const onGalleryContainerClick = evt => {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}">`,
+    {
+      onShow: (instance) => window.addEventListener("keydown", onEscKeydown),
+      onClose: (instance) =>
+        window.removeEventListener("keydown", onEscKeydown),
+    }
+  );
+  instance.show();
+
+  function onEscKeydown(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+
+
+
+
